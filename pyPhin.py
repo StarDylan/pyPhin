3df55f30490b39a78e2d8b5c3ed0976bc1443b0f
Dylan Starink <dylan@starink.com>
2020-09-08 07:40:45 -0700

    Star dylan patch 1 (#1)
    * Cleaned up static variables and fixed function parameters
    
    * Made function parameters clearer and added temp and status
    
    * Update README.md
    
    Added extra output types and added extra documentation on output of functions.
    
    * Added Copyright Credits
    
    * Updated README with Consistent Variable Names
    
    * Removed Specific Numbers
    
    * Added more Validation
    
    * Added Extra Trademark Info
    
    * Fixed Misc. Review Issues
    
    * Fixed Other Misc. Code Review Changes
    
    * Added Ability to Continue with some Errors and Added Logging
    
    * Added Pass in Logging

--- pyPhin.py
+++ pyPhin.py
@@ -1,19 +1,34 @@
+#!/usr/bin/env python3
+"""
+pyPhin by Dylan Starink - An Unoffical pHin Python API
+Copyright (C) 2020 StarDylan
+https://github.com/StarDylan/pyPhin
+
+"pHin" is a trademark owned by ConnectedYard, Inc., see www.phin.co for more information.
+I am in no way affiliated with the ConnectedYard, Inc. organization.
+
+"""
+
 import requests
 import json
-import copy
+import re
+import logging
+
 
 class pHin():
 
 	baseUrl = "https://api.phin.co"
 
-	def __init__(self):
-
+	def __init__(self, logger=None):
+		if logger != None:
+			self.logger = logger
+		else:
+			self.logger = logging.getLogger("nullLogger").addHandler(logging.NullHandler())
 
 	def login(self, contact, deviceUUID):
 
-		baseHeaders = {"x-phin-concise":"true",
-			"x-phin-reporting-app-id":"ios-app",
-			"x-phin-reporting-device-id":deviceUUID}
+		self.checkEmail(contact)
+
 
 		''' urls
 		{
@@ -32,24 +47,24 @@ class pHin():
 			"token": <token>
 		}
 		'''
-		reqJson = json.loads(requests.post(self.baseUrl+urls["signin"],
+		req = requests.post(self.baseUrl+urls["signin"],
 			json={"contact":contact,"deviceType":"python"},
-			headers=baseHeaders).text)
+			headers=self.createHeader(deviceUUID))
+
+
+		self.checkRequest(req)
+		reqJson = json.loads(req.text)
 
 
 
-		if reqJson["success"]:
-			#Returns Route to verify
-			return reqJson["verifyUrl"]
-		else:
-			raise Exception(reqJson)
+		#Returns Route needed to verify
+		return reqJson["verifyUrl"]
 
-	def verify(self, contact, verifyUrl, deviceUUID, verificationCode):
+	def verify(self, contact, deviceUUID, verifyUrl, verificationCode):
 
-		baseHeaders = {"x-phin-concise":"true",
-			"x-phin-reporting-app-id":"ios-app",
-			"x-phin-reporting-device-id":deviceUUID}
-
+		self.checkVerificationCode(verificationCode)
+		self.checkUrlRoute(verifyUrl)
+		self.checkEmail(contact)
 
 		''' verify_route
 		{
@@ -58,8 +73,8 @@ class pHin():
 			"auth_token": <auth_token>,
 			"refresh_token": <refresh_token>,
 			"user": {
-				"locationsUrl": "/users/3482/locations",
-				"userRefreshTokenUrl": "/users/3482/refreshToken",
+				"locationsUrl": "/users/1234/locations",
+				"userRefreshTokenUrl": "/users/1234/refreshToken",
 			}
 		}
 		'''
@@ -68,8 +83,10 @@ class pHin():
 			json={"contact":contact,
 				"deviceId":deviceUUID,
 				"verificationCode":verificationCode},
-			headers=baseHeaders)
+			headers=self.createHeader(deviceUUID))
 
+
+		self.checkRequest(req)
 		reqJson = json.loads(req.text)
 
 		if not reqJson["success"]:
@@ -100,7 +117,7 @@ class pHin():
 				 ]
 				"resources":{
 					"vessels": {
-						"route":"/users/.../locations/.../vessels"
+						"route":"/users/1234/locations/1234/vessels"
 					}
 				}
 			  }
@@ -109,47 +126,116 @@ class pHin():
 		'''
 		req = requests.get(
 			self.baseUrl+locationUrl,
-			headers=self.createHeader(authToken, deviceUUID, "2.0.1")
+			headers=self.createHeader(deviceUUID, authToken, "2.0.1")
 			)
+
+		self.checkRequest(req)
 		reqJson = json.loads(req.text)
 
-		if not reqJson["success"]:
-			raise Exception(reqJson)
 
 		vesselUrl = reqJson["locations"][0]["resources"]["vessels"]["route"]
 
-		auth = {"authToken":authToken,"vesselUrl":vesselUrl,"UUID":deviceUUID}
-		return json.dumps(auth)
+		#Auth Dictionary Structure needed to access data.
+		authData = {"authToken":authToken,"vesselUrl":vesselUrl}
 
-	def createHeader(self, authToken, deviceUUID, version="1.0.0"):
-		tempHeader = {"x-phin-concise":"true",
-			"x-phin-reporting-app-id":"ios-app",
-			"x-phin-reporting-device-id":deviceUUID}
-		tempHeader["Accept-Version"] = version
-		tempHeader["Authorization"] = "Bearer " + authToken
-		return tempHeader
+		return authData
 
-	def getData(self, auth, data):
+	def getData(self, authToken, deviceUUID, vesselUrl):
 
-		dataCluster = json.loads(auth)
+		self.checkUrlRoute(vesselUrl)
 
-		if data.upper() == "TA" or "CYA" or "TH":
-			return self.getWaterQuality(dataCluster["authToken"], dataCluster["vesselUrl"], dataCluster["UUID"], data.upper())
+		data = {}
 
-	def getWaterQuality(self, authToken, vesselUrl, deviceUUID, waterQualityType):
+		data["waterData"] = self.getWaterData(
+			authToken,
+			deviceUUID,
+			vesselUrl)
+
+		return data
+
+	def getWaterData(self, authToken, deviceUUID, vesselUrl):
 
 
 		req = requests.get(
 			self.baseUrl+vesselUrl,
-			headers=self.createHeader(authToken,deviceUUID, "2.0.0")
+			headers=self.createHeader(deviceUUID, authToken, "2.0.0")
 			)
+
+
+		self.checkRequest(req)
 		reqJson = json.loads(req.text)
-		print(reqJson)
 
+		data = {}
 
-		return reqJson["vessels"][0]["waterReport"][waterQualityType]["value"]
+		for dataType in ["TA","CYA","TH"]:
+			try:
+				data[dataType] = reqJson["vessels"][0]["waterReport"][dataType]["value"]
+			except:
+				self.logger.error("Not able to access %s with %s",dataType,req.text)
 
+		try:
+			data["temperature"] = reqJson["vessels"][0]["disc"]["temperatureF"]
+		except:
+			self.logger.error("Not able to access temperature with %s",req.text)
+		try:
+			data["status"] = reqJson["vessels"][0]["disc"]["title"]
+		except:
+			self.logger.error("Not able to access temperature with %s",req.text)
 
+		'''Sample Return data
+		{
+			"TA":100,
+			"CYA": 40,
+			"TH": 170
+			"temperature":70.9
+			"status":"balanced"
+		}
+		'''
+		return data
 
+	def createHeader(self, deviceUUID, authToken=None, version=None):
+		headers = {"x-phin-concise":"true",
+			"x-phin-reporting-app-id":"ios-app",
+			"x-phin-reporting-device-id":deviceUUID}
+		if version != None:
+			headers["Accept-Version"] = version
+		if authToken != None:
+			headers["Authorization"] = "Bearer " + authToken
+		return headers
 
+	def checkRequest(self, request):
+		if request == None:
+			raise Exception("Request is None!")
+		try:
+			reqJson = json.loads(request.text)
+		except:
+			raise Exception("Request is not Returning Json!")
 
+		if not reqJson["success"]:
+			raise Exception(json)
+
+	def checkUrlRoute(self, urlRoute):
+		if type(urlRoute) != str:
+			logigng.critical("Url %s not a String!",urlRoute)
+			raise Exception("Url not a String!")
+		if re.match("^\/",urlRoute) == None:
+			self.logger.critical("URL %s not Valid!", urlRoute)
+			raise Exception("Not a Valid URL Route!")
+
+	def checkEmail(self, email):
+		if type(email) != str:
+			self.logger.critical("Email %s not a String!", email)
+			raise Exception("Email not a String!")
+
+		if re.match("^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$",email) == None:
+			self.logger.critical("Email %s not Valid!",email)
+			raise Exception("Not a Valid Email!")
+
+	def checkVerificationCode(self, verificationCode):
+		if type(verificationCode) != str:
+			self.logger.critical("Verification Code %s not a String!",verificationCode)
+			raise Exception("Verification is not String!")
+
+		if not verificationCode.isnumeric():
+			self.logger.critical("Verification Code %s not Numeric")
+			raise Exception("Verification Code is not Numeric!")
